9c2c81b96ef3372b64767f94d16fd02faf0457ff
ilcomp <cgde88@gmail.com>
2015-10-16 15:45:27 +0300

    u

--- otdelkadesign.ru/app/views/site/index.php
+++ otdelkadesign.ru/app/views/site/index.php
@@ -2,9 +2,9 @@
 $this->title = 'Otdelka design';
 ?>
 			
-			<div id="fixed-main">
-				<section id="trip-grid">
-					<div class="container">
+<div id="fixed-main">
+	<section id="trip-grid">
+		<div class="container">
 <p class="text-center h2">Ремонт квартиры «под ключ» во Владимире и Владимирской области</p>
 <p>
 Ремонт квартиры «под ключ» является основным направлением компании «Отделка и Дизайн». 
@@ -73,299 +73,120 @@ $this->title = 'Otdelka design';
 </ul><p class="text-center h2">
 Фото отделки квартир выполненной нашей компанией
 </p>
-					</div>
-				<!--	<div class="trip-row">
-						<div class="trip-hf set-background" data-background="/assets/img/1.jpg">
-							<a href="/otdelka" class="trip-link"></a>
-							<div class="trip-icon"><a href="/" class="icon-trip"><img src="/images/otdelka.svg" alt="raft"/></a></div>
-							<div class="trip-text"><div class="trip-sep"></div>ОТДЕЛКА</div>
-						</div>
-
-						<div class="trip-qt set-background" data-background="/assets/img/4.jpg">
-							<a href="/design" class="trip-link"></a>
-							<div class="trip-icon"><a href="/" class="icon-trip"><img src="/images/design.png" alt="basecamp"/></a></div>
-							<div class="trip-text"><div class="trip-sep"></div>ДИЗАЙН</div>
-						</div>
-
-						<div class="trip-qt set-background" data-background="/assets/img/3.jpg">
-							<a href="/technology" class="trip-link"></a>
-							<div class="trip-icon"><a href="/" class="icon-trip"><img src="/images/technologii.png" alt="backpacking"/></a></div>
-							<div class="trip-text"><div class="trip-sep"></div>ТЕХНОЛОГИИ</div>
-						</div>
-					</div>
-
-					<div class="trip-row">
-						<div class="trip-qt set-background" data-background="/assets/img/2.jpg">
-							<a href="/partners" class="trip-link"></a>
-							<div class="trip-icon"><a href="/" class="icon-trip"><img src="/images/partners.png" alt="canoe"/></a></div>
-							<div class="trip-text"><div class="trip-sep"></div>ПАРТНЕРЫ</div>
-						</div>
-
-						<div class="trip-qt set-background" data-background="/assets/img/5.jpg" style="background-position-x: 0px;">
-							<a href="/cost" class="trip-link"></a>
-							<div class="trip-icon"><a href="/" class="icon-trip"><img src="/images/stoimost.png" alt="photography"/></a></div>
-							<div class="trip-text"><div class="trip-sep"></div>СТОИМОСТЬ</div>
-						</div>
-
-						<div class="trip-hf set-background" data-background="/assets/img/6.jpg">
-							<a href="/online" class="trip-link"></a>
-							<div class="trip-icon"><a href="/" class="icon-trip"><img src="/images/online.png" alt="custom"/></a></div>
-							<div class="trip-text"><div class="trip-sep"></div> ONLINE - КОНТРОЛЬ</div>
-						</div>
-					</div> -->
-				</section>
-			<a name="howwork"></a>
-				<section id="testimonials">
-					<div class="container">
-						<div id="destinations-text">
-						<h2>
-								<span>Как мы работаем?</span>
-								Otdelka Design
-						</h2>
-
-						<div class="heading-sep"></div>
-						</div>
-						<div id="testimonial-slider">
-							<div class="testimonial-slide" style="display: block">
-	Вы можете связаться с нами по телефону или оставить заявку на нашем сайте, заявки на сайте обрабатываются специалистами в течение дня
-							</div><!-- end .testimonial-slide -->
-														<div class="testimonial-slide" style="display: block">
-
-Наш специалист выезжает по Вашему адресу, бесплатно консультирует Вас, фиксирует все Ваши пожелания к будущему ремонту и делает замеры помещения
-							</div><!-- end .testimonial-slide -->
-							<div class="testimonial-slide" style="display: block">
-На основе Ваших пожеланий и особенностей помещения мы составим точную, развернутую смету, которая после утверждения Вами остается неизменной до окончания всех работ
-							</div><!-- end .testimonial-slide -->
-							<div class="testimonial-slide" style="display: block">
-По желанию клиента наши дизайнеры могут подготовить индивидуальный дизайн-проект любого уровня сложности
-							</div><!-- end .testimonial-slide -->
-							<div class="testimonial-slide" style="display: block">
-После утверждения сметы и дизайн-проекта мы заключаем с Вами договор, который является гарантом добросовестного исполнения наших обязательств
-							</div><!-- end .testimonial-slide -->
-							<div class="testimonial-slide" style="display: block">
-Наши мастера приступают к работе на объекте, строго соблюдая утвержденный график
-							</div><!-- end .testimonial-slide -->
-							<div class="testimonial-slide" style="display: block">
-После завершения всех работ мы подготавливаем объект к сдаче, производим финишную уборку и передаем ключи клиенту
-							</div><!-- end .testimonial-slide -->
-						</div><!-- end #testimonial-slider -->
-
-						<div id="testimonial-nav">
-							<a href="javascript:Testimonials.previous();" id="prev" class="testimonial-btn trans-bg"></a>
-							<a href="javascript:Testimonials.next();" id="next" class="testimonial-btn trans-bg"></a>
-							
-							<a href="javascript:;" class="client" data-client="Обращение клиента в компанию"><img src="/assets/img/d/1.jpg" alt="client"></a>
-							<a href="javascript:;" class="client" data-client="Выезд специалиста"><img src="/assets/img/d/3.jpg" alt="client"></a>
-							<a href="javascript:;" class="client" data-client="Составление сметы"><img src="/assets/img/d/2.jpg" alt="client"></a>
-							<a href="javascript:;" class="client" data-client="Дизайн проект"><img src="/assets/img/d/4.jpg" alt="client"></a>
-							<a href="javascript:;" class="client" data-client="Подписание договора"><img src="/assets/img/d/5.jpg" alt="client"></a>
-							<a href="javascript:;" class="client" data-client="Строительно-отделочные работы"><img src="/assets/img/d/6.jpg" alt="client"></a>
-							<a href="javascript:;" class="client" data-client="Сдача объекта"><img src="/assets/img/d/7.jpg" alt="client"></a>
-						</div><!-- end #testimonial-nav -->
-						<div id="testimonial-name">Обращение клиента в компанию</div>
-					</div><!-- end .container -->
-				</section>
-		
-		<section id="experience">
-
-					<div class="container">
-
-						<div id="experience-items"><img src="/images/sl.png" alt="experience" style="margin: 40px 0px;"></div>
-
-						<div id="experience-text">
-
-							<h2>
-								<span>Узнать больше о</span>
-								Otdelka Design
-							</h2>
-
-							<div class="heading-sep" style="background: #D97E34; margin: 26px 0px;"></div>
-
-							<p class="callout"></p><p>Для того, чтобы Вы могли больше узнать о нашей компании и составить свое личное мнение о нас 
-
-и нашем подходе к клиентам и работе, мы предлагаем Вам бесплатную выездную консультацию специалиста.</p>
-							<p id="hero-scrolldown" class="konsultacia" style="width: auto; position: relative; top: 30px;">
-	<a href="javascript:;" class="viewmore-down"><span class="round-3px trans-bg">Консультация</span></a>
-</p>
-						</div><!-- end #experience-text -->
-
-					</div><!-- end .container -->
-
-				</section>
-		
-			<a name="priem"></a>
-		<section class="container" id="hp-portfolio">
-		<div class="items grid">
-		
-			<h2><span>Почему OTDELKA DESIGN?</span></h2>
-							<div class="portfolio-item">
-					
-						<figure class="effect-layla">
-							<img src="/images/rabotapodogovoru.jpg" class="attachment-full wp-post-image" alt="Construction Thumb">							<figcaption>
-								<h2>Работа по договору</h2>
-							</figcaption>			
-						</figure>
-					
-				</div>
-							<div class="portfolio-item">
-					
-						<figure class="effect-layla">
-							<img src="/images/sovremennietehnologii.jpg" class="attachment-full wp-post-image" alt="3d_Character_Turtle_SQ">							<figcaption>
-								<h2>Использование современных материалов и технологий</h2>
-							</figcaption>			
-						</figure>
-					
-				</div>
-							<div class="portfolio-item">
-					
-						<figure class="effect-layla">
-							<img src="/images/kvalificirovanniemastera.jpg" class="attachment-full wp-post-image" alt="kvalificirovanniemastera">							<figcaption>
-								<h2>Квалифицированные мастера</h2>
-							</figcaption>			
-						</figure>
-					
-				</div>
-							<div class="portfolio-item">
-					
-						<figure class="effect-layla">
-							<img src="/images/onlinecontrol.jpg" class="attachment-full wp-post-image" alt="onlinecontrol">							<figcaption>
-								<h2>Ежедневный онлайн-контроль</h2>
-							</figcaption>			
-						</figure>
-					
-				</div>
-							<div class="portfolio-item">
-					
-						<figure class="effect-layla">
-							<img src="/images/luchzhiemateriali.jpg" class="attachment-full wp-post-image" alt="luchzhiemateriali">							<figcaption>
-								<h2>Лучшие материалы от партнеров со скидками</h2>
-							</figcaption>			
-						</figure>
-					
-				</div>
-							<div class="portfolio-item">
-					
-						<figure class="effect-layla">
-							<img src="/images/rabotapodizaynproektu.jpg" class="attachment-full wp-post-image" alt="rabotapodizaynproektu">							<figcaption>
-								<h2>Работа по дизайн проекту</h2>
-							</figcaption>			
-						</figure>
-					
-				</div>
-							<div class="portfolio-item">
-					
-						<figure class="effect-layla">
-							<img src="/images/individualniypodhod.jpg" class="attachment-full wp-post-image" alt="individualniypodhod">							<figcaption>
-								<h2>Индивидуальный подход к клиенту</h2>
-							</figcaption>			
-						</figure>
-					
-				</div>
-				<div class="portfolio-item">
-					
-						<figure class="effect-layla">
-							<img src="/images/oplatapofactu.jpg" class="attachment-full wp-post-image" alt="oplatapofactu">							<figcaption>
-								<h2>Оплата по факту выполненных работ без аванса</h2>
-							</figcaption>			
-						</figure>
-					
-				</div>
-				<div class="portfolio-item">
-					
-						<figure class="effect-layla">
-							<img src="/images/tochnayasmeta.jpg" class="attachment-full wp-post-image" alt="tochnayasmeta">							<figcaption>
-								<h2>Точная детальная смета</h2>
-							</figcaption>			
-						</figure>
-					
-				</div>
-				<div class="portfolio-item">
-					
-						<figure class="effect-layla">
-							<img src="/images/garantii.jpg" class="attachment-full wp-post-image" alt="garantii">							<figcaption>
-								<h2>Гарантия на работы</h2>
-							</figcaption>			
-						</figure>
-					
-				</div>
 		</div>
 	</section>
-			
-			
-			
-	<section id="experience" >
 
-		<div class="container">
-
-			<div id="experience-items2"><img src="/images/s2.png" alt="experience" class='pic'></div>
-
-			<div id="experience-text2" >
-
-				<h2>
-					Otdelka Design
-				</h2>
-
-				<div class="heading-sep" style="background: #D97E34; margin: 26px 0px;"></div>
-
-				<p class="callout"></p><p>Это комплексный подход к ремонту и отделке помещений. Обратившись в нашу компанию Вы получите решение большинства вопросов, возникающих при благоустройстве Вашего жилья. Сотрудничая с каждым клиентом, мы всегда учитываем его пожелания и материальные возможности. </p>
-			</div><!-- end #experience-text -->
-
-		</div><!-- end .container -->
-
-	</section>
-	
-	<a name="dop_slash"></a>
+	<a name="priem"></a>
 	<section class="container" id="hp-portfolio">
-		<div class="container">
-		<h2><span style="text-align: center">В Otdelka Design Вы также можете заказать:</span></h2>
-		<div class="items grid" style="text-align: center">
-			<div id="owl" class="owl-carousel">
-				<div class="item_owl" onclick="location.href='/natyazhnye-potolki'">
-					<div style="color: white; font-size: 13px; font-weight: bold; width: 40%; float: left; line-height: 20px; padding-top: 10%;">Натяжные потолки</div>
-					<div style="width: 60%; background: url('/images/05.jpg') 50% 0% / cover no-repeat; height: 100%; float: right;"></div>
-				</div>
-				<div class="item_owl" onclick="location.href='/plastic'">
-					<div style="color: white; font-size: 13px; font-weight: bold; width: 40%; float: left; line-height: 20px; padding-top: 10%;">Пластиковые окна</div>
-					<div style="width: 60%; background: url('/images/06.jpg') 50% 0% / cover no-repeat; height: 100%; float: right;"></div>
-				</div>
-				<div class="item_owl" onclick="location.href='/f3d'">
-					<div style="color: white; font-size: 13px; font-weight: bold; width: 40%; float: left; line-height: 20px; padding-top: 10%;">3D полы</div>
-					<div style="width: 60%; background: url('/images/07.jpg') 50% 0% / cover no-repeat; height: 100%; float: right;"></div>
-				</div>
-				<div class="item_owl" onclick="location.href='/knauf'">
-					<div style="color: white; font-size: 13px; font-weight: bold; width: 40%; float: left; line-height: 20px; padding-top: 10%;">Полы KNAUF</div>
-					<div style="width: 60%; background: url('/images/08.jpg') 50% 0% / cover no-repeat; height: 100%; float: right;"></div>
-				</div>
+		<div class="items grid">
+			<h2><span>Наши преимущества</span></h2>
+			<div class="portfolio-item">
+				<figure class="effect-layla">
+					<img src="/images/rabotapodogovoru.jpg" class="attachment-full wp-post-image" alt="Construction Thumb">							<figcaption>
+						<h2>Работа по договору</h2>
+					</figcaption>			
+				</figure>
+			</div>
+			<div class="portfolio-item">
+				<figure class="effect-layla">
+					<img src="/images/tochnayasmeta.jpg" class="attachment-full wp-post-image" alt="tochnayasmeta">							<figcaption>
+						<h2>Точная детальная смета</h2>
+					</figcaption>			
+				</figure>
+			</div>
+			<div class="portfolio-item">
+				<figure class="effect-layla">
+					<img src="/images/oplatapofactu.jpg" class="attachment-full wp-post-image" alt="oplatapofactu">							<figcaption>
+						<h2>Оплата по факту выполненных работ без аванса</h2>
+					</figcaption>			
+				</figure>
+			</div>
+			<div class="portfolio-item">
+				<figure class="effect-layla">
+					<img src="/images/onlinecontrol.jpg" class="attachment-full wp-post-image" alt="onlinecontrol">							<figcaption>
+						<h2>Ежедневный онлайн-контроль</h2>
+					</figcaption>			
+				</figure>
+			</div>
+			<div class="portfolio-item">
+				<figure class="effect-layla">
+					<img src="/images/luchzhiemateriali.jpg" class="attachment-full wp-post-image" alt="luchzhiemateriali">							<figcaption>
+						<h2>Лучшие материалы от партнеров со скидками</h2>
+					</figcaption>			
+				</figure>
 			</div>
 		</div>
-		</div>
 	</section>
 
-	</div>
-		
-		
+	<a name="howwork"></a>
+	<section id="testimonials">
+		<div class="container">
+			<div id="destinations-text">
+				<h2>Как мы работаем</h2>
 
-	<script>
-		$( document ).ready(function() {
-			var i = 1;
-			setInterval(function(){
-				i++;
-				if(i > 2) i = 1;
-			
-				$("#hero").animate({
-					opacity: 0,
-				},1000, function(){
-					$("#hero").css("background","url('/assets/img/hero-slide"+i+".jpg') top center no-repeat");
-					$("#hero").css("background-size","cover")
-				});
-				
-				$("#hero").animate({
-					opacity: 1,
-				}, 1000);
-				
-		}, 15000);
-		
+				<div class="heading-sep"></div>
+				</div>
+				<div id="testimonial-slider">
+					<div class="testimonial-slide" style="display: block">
+Вы можете связаться с нами по телефону или оставить заявку на нашем сайте, заявки на сайте обрабатываются специалистами в течение дня
+					</div><!-- end .testimonial-slide -->
+												<div class="testimonial-slide" style="display: block">
 
+Наш специалист выезжает по Вашему адресу, бесплатно консультирует Вас, фиксирует все Ваши пожелания к будущему ремонту и делает замеры помещения
+					</div><!-- end .testimonial-slide -->
+					<div class="testimonial-slide" style="display: block">
+На основе Ваших пожеланий и особенностей помещения мы составим точную, развернутую смету, которая после утверждения Вами остается неизменной до окончания всех работ
+					</div><!-- end .testimonial-slide -->
+					<div class="testimonial-slide" style="display: block">
+По желанию клиента наши дизайнеры могут подготовить индивидуальный дизайн-проект любого уровня сложности
+					</div><!-- end .testimonial-slide -->
+					<div class="testimonial-slide" style="display: block">
+После утверждения сметы и дизайн-проекта мы заключаем с Вами договор, который является гарантом добросовестного исполнения наших обязательств
+					</div><!-- end .testimonial-slide -->
+					<div class="testimonial-slide" style="display: block">
+Наши мастера приступают к работе на объекте, строго соблюдая утвержденный график
+					</div><!-- end .testimonial-slide -->
+					<div class="testimonial-slide" style="display: block">
+После завершения всех работ мы подготавливаем объект к сдаче, производим финишную уборку и передаем ключи клиенту
+					</div><!-- end .testimonial-slide -->
+				</div><!-- end #testimonial-slider -->
 
+				<div id="testimonial-nav">
+					<a href="javascript:Testimonials.previous();" id="prev" class="testimonial-btn trans-bg"></a>
+					<a href="javascript:Testimonials.next();" id="next" class="testimonial-btn trans-bg"></a>
+					
+					<a href="javascript:;" class="client" data-client="Обращение клиента в компанию"><img src="/assets/img/d/1.jpg" alt="client"></a>
+					<a href="javascript:;" class="client" data-client="Выезд специалиста"><img src="/assets/img/d/3.jpg" alt="client"></a>
+					<a href="javascript:;" class="client" data-client="Составление сметы"><img src="/assets/img/d/2.jpg" alt="client"></a>
+					<a href="javascript:;" class="client" data-client="Дизайн проект"><img src="/assets/img/d/4.jpg" alt="client"></a>
+					<a href="javascript:;" class="client" data-client="Подписание договора"><img src="/assets/img/d/5.jpg" alt="client"></a>
+					<a href="javascript:;" class="client" data-client="Строительно-отделочные работы"><img src="/assets/img/d/6.jpg" alt="client"></a>
+					<a href="javascript:;" class="client" data-client="Сдача объекта"><img src="/assets/img/d/7.jpg" alt="client"></a>
+				</div><!-- end #testimonial-nav -->
+			<div id="testimonial-name">Обращение клиента в компанию</div>
+		</div><!-- end .container -->
+	</section>
+
+</div>
+
+<script>
+$( document ).ready(function() {
+	var i = 1;
+	setInterval(function(){
+		i++;
+		if(i > 2) i = 1;
+	
+		$("#hero").animate({
+			opacity: 0,
+		},1000, function(){
+			$("#hero").css("background","url('/assets/img/hero-slide"+i+".jpg') top center no-repeat");
+			$("#hero").css("background-size","cover")
 		});
-	</script>
+		
+		$("#hero").animate({
+			opacity: 1,
+		}, 1000);
+		
+	}, 15000);
+});
+</script>
